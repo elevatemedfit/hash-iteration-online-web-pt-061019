7db5f993a2e3b9fa413baffae17f1a517ec5e848
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-24 12:05:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,10 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name,age|
-    if age<=12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-  else
-    puts "Happy Birthday #{name}! You are now #{age} years old! You are too old for this."
   end# add your code snippet here!
 end
 end
