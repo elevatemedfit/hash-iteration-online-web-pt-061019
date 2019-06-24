f65e428b768ea2eba4f19a094bc2dce92f5c751f
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-24 11:53:18 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,14 +5,14 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
+
 
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end# add the code snippet here!
 end
